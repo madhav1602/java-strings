542bdc2f763f948a9865673118702c997ca2b153
madhav1602 <madhavaggarwal580@gmail.com>
2025-03-06 06:29:55 +0530

    [Madhav] Add: checking if 2 strings are anagram are not

---
+++ AnagramCheck.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user input for two strings
+        System.out.println("Enter first string:");
+        String str1 = sc.nextLine();
+
+        System.out.println("Enter second string:");
+        String str2 = sc.nextLine();
+
+        // Check if strings are anagrams
+        if (areAnagrams(str1, str2)) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are not anagrams.");
+        }
+    }
+
+    public static boolean areAnagrams(String str1, String str2) {
+        // Remove spaces and convert to lowercase 
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        // If lengths are different, they cannot be anagrams
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Convert strings to character arrays and sort them
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        // Compare sorted arrays
+        return Arrays.equals(arr1, arr2);
+    }
+}
